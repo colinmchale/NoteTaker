1d38c1e240cda9bbb81b565b6a2c09d737b67b16
Colin McHale <93059063+colinmchale@users.noreply.github.com>
2021-12-12 22:53:19 -0600

    fixed typos on htmlRoutes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,17 +2,17 @@ const html = require('express').Router();
 const path = require('path');
 
 // GET Route for notes
-app.get('/notes', (req, res) =>
+html.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 // GET Route for homepage
-app.get('/', (req, res) =>
+html.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 // Wildcard route to direct users back to homepage
-app.get('*', (req, res) =>
+html.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
