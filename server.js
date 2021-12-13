1598246f42b6b4cc2377f2ebdc23581575de9238
Colin McHale <93059063+colinmchale@users.noreply.github.com>
2021-12-12 22:33:06 -0600

    set up initial routes from server.js

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
+const express = require('express');
 
+const api = require('./routes/apiRoutes.js');
+const html = require('./routes/htmlRoutes.js');
 
 const PORT = process.env.PORT || 3001;
 
 
+const app = express();
+
 app.use(express.static('public'));
+
+app.use('/api', api);
+app.use('/', html);
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
