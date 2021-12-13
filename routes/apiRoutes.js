914940d9586717805863e628d9ec15d2961e39d1
Colin McHale <93059063+colinmchale@users.noreply.github.com>
2021-12-12 23:08:30 -0600

    started get, post, delete request on apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,1 +1,19 @@
+const api = require('express').Router();
 
+const db = require ('../db/db.json');
+
+
+api.get('/notes', (req, res) => {
+    db.getNotes().then((notes) => res.json(notes))
+})
+
+
+api.post('/notes', (req, res) => {
+    db.saveNote(req.body).then((notes) => res.json(notes))
+})
+
+api.delete('/notes/:id', (req, res) => {
+    db.deleteNote().then((notes) => res.json(notes))
+})
+
+module.exports = api;
